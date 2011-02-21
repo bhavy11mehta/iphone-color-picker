15bde45fe7694a35851b9fdc065775b8294e150e
max.iphone.dev@gmail.com <max.iphone.dev@gmail.com@a6224330-73a6-915a-469d-0aebfd69bea9>
2011-02-21 19:11:44 +0000

    See

--- trunk/Classes/ColorPickerViewController.m
+++ trunk/Classes/ColorPickerViewController.m
@@ -13,51 +13,42 @@
 
 @implementation ColorPickerViewController
 
-@synthesize delegate, chooseButton;
-#ifdef IPHONE_COLOR_PICKER_SAVE_DEFAULT
-@synthesize defaultsKey;
-#else
-@synthesize defaultsColor;
-#endif
+@synthesize delegate, defaultsKey, chooseButton;
 
-NSString *keyForHue = @"hue";
-NSString *keyForSat = @"sat";
-NSString *keyForBright = @"bright";
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
-#ifdef IPHONE_COLOR_PICKER_SAVE_DEFAULT
 	NSUserDefaults *saveColors = [NSUserDefaults standardUserDefaults];
 	if (defaultsKey==nil) {
-        self.defaultsKey = @"";
-        NSLog(@"problem 0 in ColorPickerViewController.viewDidLoad");
+        defaultsKey = @"";
     }
-    
-    NSData *colorData= [saveColors objectForKey:defaultsKey];
-    UIColor *color;
-    if (colorData!=nil) {
-        color = (UIColor*)[NSKeyedUnarchiver unarchiveObjectWithData:colorData];
-    }
-#endif
-    
 	
-    [self moveToDefault];   // Move the crosshair to the default setting
-}
-
--(void) moveToDefault {
-  ColorPickerView *theView = (ColorPickerView*) [self view];
-  #ifdef IPHONE_COLOR_PICKER_SAVE_DEFAULT
-    NSUserDefaults *saveColors = [NSUserDefaults standardUserDefaults];
-    NSData *colorData= [saveColors objectForKey:defaultsKey];
-    UIColor *color;
-    if (colorData!=nil) {
-        color = (UIColor*)[NSKeyedUnarchiver unarchiveObjectWithData:colorData];
-    }
-    [theView setColor:color];
-  #else
-    [theView setColor:defaultsColor];
-  #endif
+	UIColor *theColor;
+	
+	ColorPickerView *theView = (ColorPickerView *)[self view];
+	
+	if ([defaultsKey isEqualToString:@"CMYColor"]) { // CMY Color
+		NSArray *componentsArray = [saveColors objectForKey:@"CMYComponents"];
+		float components[5] = {[[componentsArray objectAtIndex:0] floatValue], [[componentsArray objectAtIndex:1] floatValue], [[componentsArray objectAtIndex:2] floatValue], 0.0, 1.0};
+		
+		CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceCMYK();
+		CGColorRef color = CGColorCreate(colorSpace, components);
+		
+		theColor = [UIColor colorWithCGColor:color];
+		
+		[theView setColor:theColor];
+		
+		CGColorRelease(color);
+		CGColorSpaceRelease(colorSpace);
+	} else {
+		NSData *colorData = [saveColors objectForKey:defaultsKey];
+		if (colorData != nil) {
+			theColor = (UIColor*)[NSKeyedUnarchiver unarchiveObjectWithData:colorData];
+		}
+		
+		[theView setColor:theColor];
+	}
 }
 
 //- (void) viewWillDisappear :(BOOL)animated { 
@@ -72,54 +63,13 @@ NSString *keyForBright = @"bright";
     [delegate colorPickerViewController:self didSelectColor:[self getSelectedColor]];
 }
 
-- (IBAction) cancelColorSelection {
-    #ifdef IPHONE_COLOR_PICKER_SAVE_DEFAULT
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-	if (defaultsKey==nil) {
-        defaultsKey = @"";
-        NSLog(@"problem 0 in ColorPickerViewController.viewDidLoad");
-    }
-    
-    NSData *colorData= [userDefaults objectForKey:defaultsKey];
-    UIColor *color;
-    if (colorData!=nil) {
-        color = (UIColor*)[NSKeyedUnarchiver unarchiveObjectWithData:colorData];
-    }
-    
-    [delegate colorPickerViewController:self didSelectColor:color];
-    #else
-    [self dismissModalViewControllerAnimated:YES];
-    #endif
-}
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
     // Release anything that's not essential, such as cached data
 }
 
 
-// Housekeeping actions when a view as unloaded
-- (void)viewDidUnload {
-  // Release any retained subviews of the main view.
-#if ___IPHONE_OS_VERSION_MAX_ALLOWED >= 30000
-  [super viewDidUnload];  // First super, from iOS 3 on
-
-  self.chooseButton=nil;   // Same as release, but also setting it to nil
-#endif
-
-  return;
-}
-
-
-
 - (void)dealloc {
-    // Release claimed resources also
-#ifdef IPHONE_COLOR_PICKER_SAVE_DEFAULT
-    [defaultsKey release];
-#else
-    [defaultsColor release];
-#endif
-
     [super dealloc];
 }
 
