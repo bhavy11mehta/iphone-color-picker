16ffbac9b816058fb6c874f9344a7111f4567a05
fcanas <fcanas@a6224330-73a6-915a-469d-0aebfd69bea9>
2011-03-11 23:43:40 +0000

    Merging in some changes from ColorSwatch branch.

--- Classes/Constants.h
+++ Classes/Constants.h
@@ -29,15 +29,17 @@
 //    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-
-//#define kBrightnessGradientPlacent CGRectMake(10, 434, 300, 40)
 #define kBrightnessGradientPlacent CGRectMake(10, 404, 300, 40)
 
 #define kHueSatImage @"colormap.png"
 
-//#define kHueSatFrame CGRectMake(11, 170, 298, 257)
-//#define kColorWindowFrame CGRectMake(66, 10, 188, 44)
-#define kHueSatFrame CGRectMake(11, 120, 298, 257)
+#define kXAxisOffset 11.0
+#define kYAxisOffset 120.0
+
+#define kMatrixWidth 298.0
+#define kMatrixHeight 257.0
+
+#define kHueSatFrame CGRectMake(kXAxisOffset, kYAxisOffset, kMatrixWidth, kMatrixHeight)
 #define kColorWindowFrame CGRectMake(66, 0, 188, 44)
 
 #define kInitialBrightness 0.5
@@ -48,13 +50,5 @@
 
 #define kAnimationDuration 0.4
 
-#define kMatrixWidth 298.0
-#define kMatrixHeight 257.0
-
-#define kXAxisOffset 11.0
-//#define kYAxisOffset 170.0
-#define kYAxisOffset 120.0
-
-//#define kBrightBarYCenter 451
 #define kBrightBarYCenter 423
 
